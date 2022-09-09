3b41f56b39527c5e3341c3eae0b95f2f29a11a30
amiyasekhar <amiyasekhar@gmail.com>
2022-09-09 19:51:16 -0400

    modifying main

--- PopulationScripts/PopulateMessages.js
+++ PopulationScripts/PopulateMessages.js
@@ -1,9 +1,3 @@
-// All information, source code contained in this document 
-// is the property of StrynDev Solutions, LLC. It must not 
-// be transmitted to others without the written consent of 
-// StrynDev Solutions. It must be returned to StrynDev Solutions 
-// when its authorized use is terminated.
-
 const User = require('../models/User')
 const Message = require('../models/Message')
 const MessageChat = require('../models/MessageChat')
--- test/UserController/GetUserByUserId.test.js
+++ test/UserController/GetUserByUserId.test.js
@@ -1,9 +1,3 @@
-// All information, source code contained in this document 
-// is the property of StrynDev Solutions, LLC. It must not 
-// be transmitted to others without the written consent of 
-// StrynDev Solutions. It must be returned to StrynDev Solutions 
-// when its authorized use is terminated.
-
 const request = require('supertest');
 const { app } = require('../../server');
 const mongoose = require('mongoose');
