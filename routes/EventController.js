cf4af2ab01de9f9d456dfc4a4e05272aee8a0424
Ashu-positiveillusions <ashutosh.meddy@gmail.com>
2023-02-26 15:04:53 +0530

    event controllers added

--- routes/EventController.js
+++ routes/EventController.js
@@ -1,78 +1,102 @@
-const express = require('express');
-const { ObjectId } = require('mongodb');
+const express = require("express");
+// const { ObjectId } = require("mongodb");
 const router = express.Router();
-const Event = require('../models/Event');
+const Event = require("../models/Event");
 
-
-router.delete('/club/:eventId', async (req, res) => {
-    try {
-        let eventId = req.params.eventId;
-        const eventResults = await Event.findOneAndUpdate({ _id: eventId, isDeleted: false},{isDeleted:true}, {new : true});
-        if(!eventResults) return res.status(400).send({status: false, message: "No events found"});
-        return res.status(200).send({status: true, message: "successfull deleted"});
-    } catch (err) {
-        return res.status(500).send({ message:err.message});
-    }
-
+router.get("/club/:clubid", async (req, res) => {
+  try {
+    let clubId = req.params.clubid;
+    const eventResults = await Event.find({ clubId: clubId, isDeleted: false });
+    if (!eventResults.length)
+      return res
+        .status(400)
+        .send({ status: false, message: "No events found for the club" });
+    return res
+      .status(200)
+      .send({ status: true, message: "success", data: eventResults });
+  } catch (err) {
+    return res.status(500).send({
+      message:
+        "Invalid request - were not able to retreive event information from the given club",
+    });
+  }
 });
 
-router.get('/club/:clubid', async (req, res) => {
-    try {
-        let clubId = req.params.clubid;
-        const eventResults = await Event.find({ clubId: clubId, isDeleted: false});
-        if(!eventResults.length) return res.status(400).send({status: false, message: "No events found for the club"});
-        return res.status(200).send({status: true, message: "success", data: eventResults});
-    } catch (err) {
-        return res.status(500).send({ message: "Invalid request - were not able to retreive event information from the given club"});
-    }
+router.get("/club/:clubId/:eventId", async (req, res) => {
+  try {
+    let { clubId, eventId } = req.params;
+    let event = await Event.findOne({
+      clubId: clubId,
+      _id: eventId,
+      isDeleted: false,
+    }).lean();
+    if (!event)
+      return res
+        .status(400)
+        .send({ status: false, message: "event not found" });
+
+    return res
+      .status(200)
+      .send({ status: true, message: "event found", data: event });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
 });
 
+router.post("/club/:clubid", async (req, res) => {
+  try {
+    const clubId = req.params.clubid;
+    let eventBody = req.body;
+    let event = await Event.create(eventBody);
+    return res.status(201).send({
+      status: true,
+      message: "event created successfully",
+      data: event,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      message: `Invalid request -- We were unable to create a new event`,
+    });
+  }
+});
 
-// router.post('/club/:clubid', async (req, res) => {
-//     const clubIdParam = req.params.clubid;
+router.put("club/:clubId/:eventId", async (req, res) => {
+  try {
+    let { clubId, eventId } = req.params;
+    let updatedClubData = req.body;
+    let updatedClub = await Event.findOneAndUpdate(
+      { clubId: clubId, _id: eventId, isDeleted: false },
+      updatedClubData,
+      { new: true }
+    );
+    if (!updatedClub)
+      return res.status(400).send({ status: false, message: "not found" });
+    return res.status(200).send({
+      status: true,
+      message: "successfully updated",
+      data: updatedClub,
+    });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+});
 
-//     let nameParam = null;
-//     let picParam = null;
-//     let dateParam = null;
-//     let timeParam = null;
-//     let ticketLinkParam = null;
-
-//     try {
-
-//         nameParam = req.body.name;
-//         picParam = req.body.picture;
-//         dateParam = req.body.eventDate;
-//         timeParam = req.body.eventTime;
-//         ticketLinkParam = req.body.ticketLink;
-
-//     } catch (err) {
-//         res.status(400).send({ message: `Invalid request -- We were unable to create a new event`});
-//         return;
-
-//     }
-
-
-//     let newEvent = null;
-
-//     try {
-
-//         newEvent = await Event.create({
-//             name: nameParam,
-//             picture: picParam,
-//             eventDate: dateParam,
-//             eventTime: timeParam,
-//             ticketLink: ticketLinkParam,
-//             clubId: clubIdParam
-//         });
-    
-//         await newEvent.save();
-//         res.json({ message: "The event was successfully added"});
-//         return;
-
-//     } catch (err) {
-//         res.status(400).send({ message: `Invalid request -- We were unable to create a new event`});
-//         return;
-//     }
-// });
+router.delete("club/:clubId/:eventId", async (req, res) => {
+  try {
+    let { clubId, eventId } = req.params;
+    let deletedClub = await Event.findOneAndUpdate(
+      { clubId: clubId, _id: eventId, isDeleted: false },
+      { isDeleted: true },
+      { new: true }
+    );
+    if (!deletedClub)
+      return res.status(400).send({ status: false, message: "not found" });
+    return res
+      .status(200)
+      .send({ status: true, messages: "successfully deleted" });
+  } catch (error) {
+    return res.status(500).send({ status: false, message: error.message });
+  }
+});
 
 module.exports = router;
--- routes/TableConfigurationController.js
+++ routes/TableConfigurationController.js
@@ -1,12 +1,5 @@
-// All information, source code contained in this document 
-// is the property of StrynDev Solutions, LLC. It must not 
-// be transmitted to others without the written consent of 
-// StrynDev Solutions. It must be returned to StrynDev Solutions 
-// when its authorized use is terminated.
-
-
 const express = require('express');
-const { ObjectId } = require('mongodb');
+// const { ObjectId } = require('mongodb');
 const router = express.Router();
 const TableConfiguration = require('../models/TableConfiguration');
 
