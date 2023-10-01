e564da9eb017ec29e2edafde6c79e97c04780da1
Vishesh Gupta <55517509+guptavishesh143@users.noreply.github.com>
2023-10-01 22:17:24 +0530

    port changed to 4000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
 const socketio = require("socket.io");
 const cors = require("cors");
