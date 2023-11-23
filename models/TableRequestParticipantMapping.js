254fe3bc55f84e5057bbc4f43945912397162327
amiyasekhar <amiyasekhar@gmail.com>
2023-11-23 02:44:27 -0500

    fixed null participant issue

--- models/TableRequestParticipantMapping.js
+++ models/TableRequestParticipantMapping.js
@@ -10,7 +10,7 @@ const tableRequestParticipantMappingSchema = new Schema({
   },
   participantId: {
     type: Schema.Types.ObjectId,
-    ref: "User",
+    ref: "Participant",
     required: true,
   },
   minimumPrice: {
